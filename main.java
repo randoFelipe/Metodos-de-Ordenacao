61412b6d5aa8aef16585d0065fc6f8520def2a9d
Felipe Rando <felipe.rando@rivendel.com.br>
2017-10-23 10:27:52 -0200

    testando

--- main.java
+++ main.java
@@ -11,6 +11,7 @@ public class main {
         /*for (int numero : array) {
             System.out.print(numero+"; ");
         }*/
+        
         System.out.println(measureTime()+" segundos");
     }
 
@@ -29,7 +30,24 @@ public class main {
         long endTime = System.nanoTime();
         long total = endTime - startTime;
         return total;
-    }    
+    }
+    public static void bubbleSort(int[] numArray) {
+        
+        int n = numArray.length;
+        int temp = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+        
+                if (numArray[j - 1] > numArray[j]) {
+                    temp = numArray[j - 1];
+                    numArray[j - 1] = numArray[j];
+                    numArray[j] = temp;
+                    }
+        
+                }
+            }
+        }
 }
 
 
